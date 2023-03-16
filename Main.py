e70d9e4a68163dd203fd5470f0b2087523ceada1
OssamaTammam <ossamatammam@gmail.com>
2023-03-16 23:36:48 +0200

    Bug fixes

--- Main.py
+++ Main.py
@@ -32,7 +32,7 @@ def copyArray(original, clone):
 
 
 if __name__ == '__main__':
-    originalArray = generateRandomArray(1000)
+    originalArray = generateRandomArray(10000)
     tempArray = []
     print(f"Original array is {originalArray}")
     print("---------------------------------------------------------------------------")
@@ -106,11 +106,11 @@ if __name__ == '__main__':
     # find kth smallest element algorithm
     copyArray(originalArray, tempArray)
     start = time.time()
-    k = QuickSort.findKthElement(tempArray, 0, len(tempArray) - 1, 3)
+    k = QuickSort.findKthElement(tempArray, 0, len(tempArray) - 1, 200)
     end = time.time()
     runningTime = (end - start) * 1000
     print(
-        f"Running time for Finding Kth Smallest Element is {runningTime} ms \nKth smallest element is {k}")
+        f"Running time for Finding Kth (200th) Smallest Element is {runningTime} ms \nKth smallest element is {k}")
     print("---------------------------------------------------------------------------")
     copyArray(originalArray, tempArray)
 
@@ -121,6 +121,8 @@ if __name__ == '__main__':
     insertionSortTime = []
     arraySizes = []
     arraySize = 1000
+
+    calcTimeStart = time.time()
 
     while arraySize <= 10000:
         originalArray = generateRandomArray(arraySize)
@@ -173,6 +175,7 @@ if __name__ == '__main__':
         arraySizes.append(arraySize)
         arraySize += 1000
 
+    calcTimeEnd = time.time()
     df = pd.DataFrame({'Input Size (N)': arraySizes,
                        'Quick Sort': quickSortTime,
                        'Merge Sort': mergeSortTime,
@@ -181,4 +184,4 @@ if __name__ == '__main__':
                        'Insertion Sort': insertionSortTime}).T
     df.to_excel(excel_writer="running_times.xlsx")
 
-    print("Done")
+    print(f"Time for all calculations is {calcTimeEnd - calcTimeStart} seconds")
