a4ec72fe7dbd2e85ee1579f5765481c47d88cf4c
OssamaTammam <ossamatammam@gmail.com>
2023-04-01 02:26:44 +0200

    Wrote a simple text based main

---
+++ Assignment 2 Red Black Tree/Main.py
@@ -0,0 +1,18 @@
+import Dictionary
+
+if __name__ == '__main__':
+    dictionary = Dictionary.Dictionary("dictionary")
+    while True:
+        print("1.Insert word\n2.Search for a word\n3.Print dictionary size\n4.Exit")
+        choice = int(input(f"Choose your desired operation: "))
+        match choice:
+            case 1:
+                word = input("Enter the desired word: ")
+                dictionary.insert(word.strip())
+            case 2:
+                word = input("Enter the desired word: ")
+                dictionary.search(word.strip())
+            case 3:
+                dictionary.printSize()
+            case 4:
+                break
