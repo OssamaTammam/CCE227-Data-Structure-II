d0e97690c88448b0eea1dd3113770cb67ae7138f
OssamaTammam <ossamatammam@gmail.com>
2023-05-19 17:44:42 +0300

    Implemented Dijkstra algorithm

--- Assignment 3 Graphs/Graph.py
+++ Assignment 3 Graphs/Graph.py
@@ -1,4 +1,5 @@
 import heapq
+import sys
 
 
 class Graph:
@@ -46,3 +47,44 @@ class Graph:
                 heapq.heappush(heap, (edge[0], v, edge[1]))
 
         return mst
+
+    def dijkstra(self, starting_vertex):
+        if starting_vertex not in self.graph:
+            return
+
+        visited = set()  # keep track of visited vertices
+        heap = []  # priority heap to get the shortest distance
+        distances = {}  # dictionary to store shortest distance between starting vertex and all other vertices
+        path = {}  # dictionary that stores paths from starting vertex to all other vertices
+
+        # initialize with max distance at every vertex
+        for vertex in self.graph:
+            distances[vertex] = sys.maxsize
+            path[vertex] = []
+
+        distances[starting_vertex] = 0
+
+        heapq.heappush(heap, (0, starting_vertex))
+
+        while len(heap) > 0:
+            weight, vertex = heapq.heappop(heap)
+
+            if vertex in visited:
+                continue
+
+            visited.add(vertex)
+
+            for edge in self.graph[vertex]:
+                # edge[0] is the weight and edge[1] is the neighbor
+                if distances[vertex] + edge[0] < distances[edge[1]]:
+                    distances[edge[1]] = distances[vertex] + edge[0]
+                    path[edge[1]] = path[vertex] + [vertex]
+                    heapq.heappush(heap, (distances[edge[1]], edge[1]))
+
+        return distances, path
+
+    def shortest_path(self, starting_vertex, destination_vertex):
+        if starting_vertex not in self.graph or destination_vertex not in self.graph:
+            return
+        distances, path = self.dijkstra(starting_vertex)
+        return distances[destination_vertex], path[destination_vertex] + [destination_vertex]
